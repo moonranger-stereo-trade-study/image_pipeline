7e312a0abb85f2f7ee3ec7529d4b457e0dd44e54
AnirudhKrishna97 <anirudhkrishna007@gmail.com>
2020-11-29 22:36:08 -0500

    Bad MAKEFILE, fixed Make, errors in finish_processing

--- stereo_image_proc/src/libstereo_image_proc/process_disparity.cpp
+++ stereo_image_proc/src/libstereo_image_proc/process_disparity.cpp
@@ -50,7 +50,7 @@ static void ProcessDisparity::processDisparity(const cv::Mat& left_rect, const c
   static const double inv_dpp = 1.0 / DPP;
 
   // Block matcher produces 16-bit signed (fixed point) disparity image
-  if (current_stereo_algorithm_ == BM)
+  if (current_stereo_algorithm == BM)
 #if CV_MAJOR_VERSION >= 3
     block_matcher->compute(left_rect, right_rect, disparity16);
   else
